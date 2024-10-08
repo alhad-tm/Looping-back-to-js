8d3b92cea97fea08717a6af09f81e6792048a1ec
alhad-tm <alhadchikkutm@gmail.com>
2024-10-08 11:22:09 +0530

    functions

---
+++ jsFundamentals.js
@@ -0,0 +1,45 @@
+//Functions
+
+function myFunc() {
+  let a = 26;
+  let b = 77;
+  c = a + b;
+  console.log(c);
+}
+
+myFunc();
+
+function MyNewFunc() {
+  let d = 22;
+  return d;
+}
+
+console.log(MyNewFunc());
+
+function newFunc() {
+  let a = 321;
+  completeFn();
+  return a;
+}
+
+function completeFn() {
+  console.log("completed");
+}
+
+console.log(newFunc());
+
+
+//passing parameter
+
+function addFunction(val){
+    let a=111;
+    let b=val;
+    c=a+b;
+
+    return c;
+
+}
+
+console.log(addFunction(12));
+
+
